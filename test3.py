9815b2de3a273f03f890096701f73dceb6114590
Your Name Here <asmitkumar11@gmail.com>
2015-02-10 16:27:34 +0530

    Added config file and accessing it in test3.py

--- test3.py
+++ test3.py
@@ -1,4 +1,9 @@
+import json
 from bottle import route, run
+
+config_file = open( 'config.json' )
+config_data = json.load( config_file )
+pth_xml     = config_data["paths"]["xml"]
 
 @route('/recipes/')
 def recipes_list():
